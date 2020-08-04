63315d8ace2c0b881bf08e8c673f50af162f1b05
felipe <feliperarnau@gmail.com>
2020-08-04 16:13:29 -0300

    Updates

---
+++ precache-manifest.ce261647d7d20fc74a32defb52bd0481.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6862d3fd99700e3bd7c6094cef3463a",
+    "url": "/hotels/index.html"
+  },
+  {
+    "revision": "806517a0eb1b11fb338f",
+    "url": "/hotels/static/css/main.a8eb5d9c.chunk.css"
+  },
+  {
+    "revision": "6fd2d213e30f2161eb69",
+    "url": "/hotels/static/js/2.b7dc13cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hotels/static/js/2.b7dc13cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "806517a0eb1b11fb338f",
+    "url": "/hotels/static/js/main.20baa7cf.chunk.js"
+  },
+  {
+    "revision": "63c70fd8c3c479bdeedb",
+    "url": "/hotels/static/js/runtime-main.5511927f.js"
+  }
+]);
